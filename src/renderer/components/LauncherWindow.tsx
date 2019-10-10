51d5b601625533e4248c5c253b18a9e7c0fb2a47
Arkadiusz Lach <lach@includespark.eu>
2019-10-10 20:07:15 +0200

    LauncherWindow update

--- src/renderer/components/LauncherWindow.tsx
+++ src/renderer/components/LauncherWindow.tsx
@@ -1,13 +1,22 @@
+import { remote } from 'electron';
 import * as React from 'react';
 
 import TitleBar from '../utils/TitleBar';
 import './LauncherWindow.scss';
 
-const LauncherWindow = () => (
+const LauncherWindow = () => {
+  React.useEffect(() => {
+    const window = remote.getCurrentWindow();
+    window.setResizable(true);
+    window.setSize(800, 600, true);
+    window.center();
+  });
+  return (
     <div>
-        <TitleBar resize={true}/>
-        <h1>Launcher Window</h1>
+      <TitleBar resize={true} />
+      <h1>Launcher Window</h1>
     </div>
-);
+  );
+};
 
 export default LauncherWindow;
