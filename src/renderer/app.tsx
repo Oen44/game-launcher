40d5b20fc3bb17e1f293f658170ef66a55d9b964
Arkadiusz Lach <lach@includespark.eu>
2019-10-10 18:11:37 +0200

    Rewritten code to use hookrouter

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,25 +1,26 @@
+import { useRoutes } from 'hookrouter';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
 import './global.scss';
 
+import LauncherWindow from './components/LauncherWindow';
 import LoginWindow from './components/LoginWindow';
 
-// Create main element
 const mainElement = document.createElement('div');
 document.body.appendChild(mainElement);
 document.addEventListener('dragover', event => event.preventDefault());
 document.addEventListener('drop', event => event.preventDefault());
 
-// Render components
-const render = (Component: () => JSX.Element) => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component />
-        </AppContainer>,
-        mainElement
-    );
+const routes = {
+    '/': () => <LoginWindow />,
+    '/launcher': () => <LauncherWindow />
 };
 
-render(LoginWindow);
+const App = () => {
+    const routeResult = useRoutes(routes);
+    return <AppContainer>{routeResult}</AppContainer>;
+};
+
+ReactDOM.render(<App />, mainElement);
