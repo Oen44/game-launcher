dea01d1e44ce437c61f905ec3d9d6645d06f55f5
Arkadiusz Lach <lach@includespark.eu>
2019-10-10 18:17:19 +0200

    Temporary navigation

--- src/renderer/components/LoginWindow.tsx
+++ src/renderer/components/LoginWindow.tsx
@@ -1,4 +1,5 @@
 import { remote } from 'electron';
+import { navigate } from 'hookrouter';
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
 
@@ -7,38 +8,43 @@ import LinkExternal from '../utils/LinkExternal';
 import TitleBar from '../utils/TitleBar';
 import './LoginWindow.scss';
 
-const LoginWindow = () => (
-    <div>
-        <TitleBar resize={false}/>
+const LoginWindow = () => {
+    function openLauncherWindow() {
+        navigate('launcher');
+    }
+    return (
+        <div>
+            <TitleBar resize={false} />
 
-        <div id="main">
-            <img src={logo} width="200" />
+            <div id="main">
+                <img src={logo} width="200" />
 
-            <div className="topLabel">PLEASE LOG IN</div>
+                <div className="topLabel">PLEASE LOG IN</div>
 
-            <div className="inputs">
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+                <div className="inputs">
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
 
-                <div className="checkbox-container">
-                    <label className="checkbox-label" htmlFor="rememberMe">
-                        <input type="checkbox" id="rememberMe" />
-                        <span className="checkbox-custom" />
-                        <span className="input-title">Keep me logged in</span>
-                    </label>
+                    <div className="checkbox-container">
+                        <label className="checkbox-label" htmlFor="rememberMe">
+                            <input type="checkbox" id="rememberMe" />
+                            <span className="checkbox-custom" />
+                            <span className="input-title">Keep me logged in</span>
+                        </label>
+                    </div>
+                    <button onClick={openLauncherWindow}>Log in to Launcher</button>
                 </div>
-                <button>Log in to Launcher</button>
+                <div className="line" />
+                <LinkExternal title="Can't log in?" href="https://github.com/Oen44/game-launcher" />
+                <LinkExternal
+                    title="Register new account"
+                    href="https://github.com/Oen44/game-launcher"
+                />
+                <LinkExternal title="Visit website" href="https://github.com/Oen44/game-launcher" />
+                <small>v{remote.app.getVersion()}</small>
             </div>
-            <div className="line" />
-            <LinkExternal title="Can't log in?" href="https://github.com/Oen44/game-launcher" />
-            <LinkExternal
-                title="Register new account"
-                href="https://github.com/Oen44/game-launcher"
-            />
-            <LinkExternal title="Visit website" href="https://github.com/Oen44/game-launcher" />
-            <small>v{remote.app.getVersion()}</small>
         </div>
-    </div>
-);
+    );
+};
 
 export default hot(LoginWindow);
