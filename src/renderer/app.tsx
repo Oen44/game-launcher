f591e6af9e22818e3b46b93828ced2b7466ebe78
Arkadiusz Lach <lach@includespark.eu>
2019-10-10 17:06:22 +0200

    Prevent drag&drop events

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -9,6 +9,8 @@ import LoginWindow from './components/LoginWindow';
 // Create main element
 const mainElement = document.createElement('div');
 document.body.appendChild(mainElement);
+document.addEventListener('dragover', event => event.preventDefault());
+document.addEventListener('drop', event => event.preventDefault());
 
 // Render components
 const render = (Component: () => JSX.Element) => {
