a065b0204873ab39e21386759e4fc8ee6f605290
Arkadiusz Lach <lach@includespark.eu>
2019-10-11 12:22:53 +0200

    LinkExternal component finished

--- src/renderer/utils/LinkExternal.tsx
+++ src/renderer/utils/LinkExternal.tsx
@@ -1,16 +1,22 @@
 import { shell } from 'electron';
 import * as React from 'react';
 
-const LinkExternal = ({ href, title }: { [key: string]: any }) => {
-    const openExternal = (e: React.MouseEvent) => {
-        e.preventDefault();
-        shell.openExternal(href);
-    };
-    return (
-        <a href="#" className="linkExternal" onClick={openExternal}>
-            {title}
-        </a>
-    );
+/**
+ * LinkExternal component
+ * @description Creates <a> element that opens links in default system browser
+ * @param href  Link to open
+ * @param title Text to show
+ */
+const LinkExternal = ({ href, title }: { href: string; title: string }) => {
+  const openExternal = (e: React.MouseEvent) => {
+    e.preventDefault();
+    shell.openExternal(href);
+  };
+  return (
+    <a href="#" className="linkExternal" onClick={openExternal}>
+      {title}
+    </a>
+  );
 };
 
 export default LinkExternal;
